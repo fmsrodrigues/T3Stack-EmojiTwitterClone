f9d9388407bb50f89d86534615652b7d8aa9e7cc
fmsrodrigues <filipe_msr@hotmail.com>
2023-04-02 20:57:38 -0300

    profile page with ssg helper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,17 @@ import { Toaster } from "react-hot-toast";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>T3 Emoji Twitter Clone</title>
+        <meta name="description" content="🚀" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
